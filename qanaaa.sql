065df7b461497b52fc7312eb0a080ac944d2e6d0
gitzuzu <Zeina2101569@miuegypt.edu.eg>
2024-03-18 01:58:47 +0200

    signup-functionality

--- qanaaa.sql
+++ qanaaa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 16, 2024 at 03:29 PM
+-- Generation Time: Mar 17, 2024 at 09:32 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -18,38 +18,38 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `qanaaa`
+-- Database: qanaa
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cart`
+-- Table structure for table cart
 --
 
-CREATE TABLE `cart` (
-  `cart_id` bigint(20) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `item_id` int(11) NOT NULL,
-  `quantity` int(3) NOT NULL
+CREATE TABLE cart (
+  cart_id bigint(20) NOT NULL,
+  user_id int(11) NOT NULL,
+  item_id int(11) NOT NULL,
+  quantity int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `categories`
+-- Table structure for table categories
 --
 
-CREATE TABLE `categories` (
-  `category_id` bigint(20) NOT NULL,
-  `category_name` varchar(250) NOT NULL
+CREATE TABLE categories (
+  category_id bigint(20) NOT NULL,
+  category_name varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `categories`
+-- Dumping data for table categories
 --
 
-INSERT INTO `categories` (`category_id`, `category_name`) VALUES
+INSERT INTO categories (category_id, category_name) VALUES
 (1, 'Health and Wellness'),
 (2, 'Personal Care'),
 (3, 'Baby and Child Care'),
@@ -64,126 +64,125 @@ INSERT INTO `categories` (`category_id`, `category_name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `item`
+-- Table structure for table item
 --
 
-CREATE TABLE `item` (
-  `item_id` int(5) NOT NULL,
-  `item_title` varchar(250) NOT NULL,
-  `item_brand` varchar(250) NOT NULL,
-  `item_cat` varchar(250) NOT NULL,
-  `item_details` text NOT NULL,
-  `item_quantity` int(3) NOT NULL,
-  `item_price` int(10) NOT NULL,
-  `item_offers` int(1) DEFAULT 0
+CREATE TABLE item (
+  item_id int(5) NOT NULL,
+  item_title varchar(250) NOT NULL,
+  item_brand varchar(250) NOT NULL,
+  item_cat varchar(250) NOT NULL,
+  item_details text NOT NULL,
+  item_quantity int(3) NOT NULL,
+  item_price int(10) NOT NULL,
+  item_offers int(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `item_images`
+-- Table structure for table item_images
 --
 
-CREATE TABLE `item_images` (
-  `image_id` bigint(20) NOT NULL,
-  `item_id` int(5) NOT NULL,
-  `image_path` varchar(250) NOT NULL
+CREATE TABLE item_images (
+  image_id bigint(20) NOT NULL,
+  item_id int(5) NOT NULL,
+  image_path varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `locations`
+-- Table structure for table locations
 --
 
-CREATE TABLE `locations` (
-  `id` bigint(20) NOT NULL,
-  `branch_name` varchar(255) DEFAULT NULL,
-  `address` varchar(255) DEFAULT NULL,
-  `google_maps_url` varchar(255) DEFAULT NULL
+CREATE TABLE locations (
+  id bigint(20) NOT NULL,
+  branch_name varchar(255) DEFAULT NULL,
+  address varchar(255) DEFAULT NULL,
+  google_maps_url varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `locations`
+-- Dumping data for table locations
 --
 
-INSERT INTO `locations` (`id`, `branch_name`, `address`, `google_maps_url`) VALUES
+INSERT INTO locations (id, branch_name, address, google_maps_url) VALUES
 (1, 'Nasr City', 'Al Manteqah Al Oula, Nasr City, Cairo Governorate 4450115', 'https://maps.app.goo.gl/gmTuJGybmFszvsUg9');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `notifications`
+-- Table structure for table notifications
 --
 
-CREATE TABLE `notifications` (
-  `notification_id` bigint(20) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `notification_text` varchar(250) NOT NULL,
-  `notification_date` timestamp NOT NULL DEFAULT current_timestamp()
+CREATE TABLE notifications (
+  notification_id bigint(20) NOT NULL,
+  user_id int(11) NOT NULL,
+  notification_text varchar(250) NOT NULL,
+  notification_date timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `orders`
+-- Table structure for table orders
 --
 
-CREATE TABLE `orders` (
-  `order_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `order_date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `order_status` tinyint(1) NOT NULL DEFAULT 0
+CREATE TABLE orders (
+  order_id int(11) NOT NULL,
+  user_id int(11) NOT NULL,
+  order_date timestamp NOT NULL DEFAULT current_timestamp(),
+  order_status tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `order_items`
+-- Table structure for table order_items
 --
 
-CREATE TABLE `order_items` (
-  `order_item_id` bigint(20) NOT NULL,
-  `order_id` int(11) NOT NULL,
-  `item_id` int(11) NOT NULL,
-  `item_quantity` int(3) NOT NULL
+CREATE TABLE order_items (
+  order_item_id bigint(20) NOT NULL,
+  order_id int(11) NOT NULL,
+  item_id int(11) NOT NULL,
+  item_quantity int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Table structure for table user
 --
 
-CREATE TABLE `user` (
-  `user_Lname` varchar(20) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `user_password` varchar(255) NOT NULL,
-  `user_id` int(3) NOT NULL,
-  `user_fname` varchar(20) NOT NULL,
-  `user_address` text NOT NULL,
-  `user_isAdmin` tinyint(1) NOT NULL DEFAULT 0,
-  `user_is_admin` bit(1) NOT NULL,
-  `user_user_is_admin` bit(1) NOT NULL
+CREATE TABLE user (
+  user_Lname varchar(20) NOT NULL,
+  email varchar(50) NOT NULL,
+  user_password varchar(255) NOT NULL,
+  user_id int(3) NOT NULL,
+  user_fname varchar(20) NOT NULL,
+  user_address text NOT NULL,
+  user_isAdmin tinyint(1) NOT NULL DEFAULT 0,
+  user_user_is_admin bit(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table user
 --
 
-INSERT INTO `user` (`user_Lname`, `email`, `user_password`, `user_id`, `user_fname`, `user_address`, `user_isAdmin`, `user_is_admin`, `user_user_is_admin`) VALUES
-('Aziz', 'georgeayman2003@gmail.com', '$2a$10$vUdrqBMjDcPkURNLJ8oIJeAMPZ3D9OJq9bTwbUjXJMR2M6N1BXDpe', 87, 'George', 'tawnyat', 1, b'0', b'0');
+INSERT INTO user (user_Lname, email, user_password, user_id, user_fname, user_address, user_isAdmin, user_user_is_admin) VALUES
+('Aziz', 'georgeayman2003@gmail.com', '$2a$10$vUdrqBMjDcPkURNLJ8oIJeAMPZ3D9OJq9bTwbUjXJMR2M6N1BXDpe', 87, 'George', 'tawnyat', 1, b'0');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `wishlist`
+-- Table structure for table wishlist
 --
 
-CREATE TABLE `wishlist` (
-  `wishlist_id` bigint(20) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `item_id` int(11) NOT NULL
+CREATE TABLE wishlist (
+  wishlist_id bigint(20) NOT NULL,
+  user_id int(11) NOT NULL,
+  item_id int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -191,180 +190,180 @@ CREATE TABLE `wishlist` (
 --
 
 --
--- Indexes for table `cart`
+-- Indexes for table cart
 --
-ALTER TABLE `cart`
-  ADD PRIMARY KEY (`cart_id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `item_id` (`item_id`);
+ALTER TABLE cart
+  ADD PRIMARY KEY (cart_id),
+  ADD KEY user_id (user_id),
+  ADD KEY item_id (item_id);
 
 --
--- Indexes for table `categories`
+-- Indexes for table categories
 --
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`category_id`);
+ALTER TABLE categories
+  ADD PRIMARY KEY (category_id);
 
 --
--- Indexes for table `item`
+-- Indexes for table item
 --
-ALTER TABLE `item`
-  ADD PRIMARY KEY (`item_id`);
+ALTER TABLE item
+  ADD PRIMARY KEY (item_id);
 
 --
--- Indexes for table `item_images`
+-- Indexes for table item_images
 --
-ALTER TABLE `item_images`
-  ADD PRIMARY KEY (`image_id`),
-  ADD KEY `item_images_ibfk_1` (`item_id`);
+ALTER TABLE item_images
+  ADD PRIMARY KEY (image_id),
+  ADD KEY item_images_ibfk_1 (item_id);
 
 --
--- Indexes for table `locations`
+-- Indexes for table locations
 --
-ALTER TABLE `locations`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE locations
+  ADD PRIMARY KEY (id);
 
 --
--- Indexes for table `notifications`
+-- Indexes for table notifications
 --
-ALTER TABLE `notifications`
-  ADD PRIMARY KEY (`notification_id`),
-  ADD KEY `user_id` (`user_id`);
+ALTER TABLE notifications
+  ADD PRIMARY KEY (notification_id),
+  ADD KEY user_id (user_id);
 
 --
--- Indexes for table `orders`
+-- Indexes for table orders
 --
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`order_id`),
-  ADD KEY `user_id` (`user_id`);
+ALTER TABLE orders
+  ADD PRIMARY KEY (order_id),
+  ADD KEY user_id (user_id);
 
 --
--- Indexes for table `order_items`
+-- Indexes for table order_items
 --
-ALTER TABLE `order_items`
-  ADD PRIMARY KEY (`order_item_id`),
-  ADD KEY `item_id` (`item_id`),
-  ADD KEY `order_items_ibfk_1` (`order_id`);
+ALTER TABLE order_items
+  ADD PRIMARY KEY (order_item_id),
+  ADD KEY item_id (item_id),
+  ADD KEY order_items_ibfk_1 (order_id);
 
 --
--- Indexes for table `user`
+-- Indexes for table user
 --
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_id`);
+ALTER TABLE user
+  ADD PRIMARY KEY (user_id);
 
 --
--- Indexes for table `wishlist`
+-- Indexes for table wishlist
 --
-ALTER TABLE `wishlist`
-  ADD PRIMARY KEY (`wishlist_id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `item_id` (`item_id`);
+ALTER TABLE wishlist
+  ADD PRIMARY KEY (wishlist_id),
+  ADD KEY user_id (user_id),
+  ADD KEY item_id (item_id);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `cart`
+-- AUTO_INCREMENT for table cart
 --
-ALTER TABLE `cart`
-  MODIFY `cart_id` bigint(20) NOT NULL AUTO_INCREMENT;
+ALTER TABLE cart
+  MODIFY cart_id bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `categories`
+-- AUTO_INCREMENT for table categories
 --
-ALTER TABLE `categories`
-  MODIFY `category_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+ALTER TABLE categories
+  MODIFY category_id bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT for table `item`
+-- AUTO_INCREMENT for table item
 --
-ALTER TABLE `item`
-  MODIFY `item_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+ALTER TABLE item
+  MODIFY item_id int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
--- AUTO_INCREMENT for table `item_images`
+-- AUTO_INCREMENT for table item_images
 --
-ALTER TABLE `item_images`
-  MODIFY `image_id` bigint(20) NOT NULL AUTO_INCREMENT;
+ALTER TABLE item_images
+  MODIFY image_id bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `locations`
+-- AUTO_INCREMENT for table locations
 --
-ALTER TABLE `locations`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE locations
+  MODIFY id bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `notifications`
+-- AUTO_INCREMENT for table notifications
 --
-ALTER TABLE `notifications`
-  MODIFY `notification_id` bigint(20) NOT NULL AUTO_INCREMENT;
+ALTER TABLE notifications
+  MODIFY notification_id bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `orders`
+-- AUTO_INCREMENT for table orders
 --
-ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=217;
+ALTER TABLE orders
+  MODIFY order_id int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=217;
 
 --
--- AUTO_INCREMENT for table `order_items`
+-- AUTO_INCREMENT for table order_items
 --
-ALTER TABLE `order_items`
-  MODIFY `order_item_id` bigint(20) NOT NULL AUTO_INCREMENT;
+ALTER TABLE order_items
+  MODIFY order_item_id bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT for table user
 --
-ALTER TABLE `user`
-  MODIFY `user_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=88;
+ALTER TABLE user
+  MODIFY user_id int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=88;
 
 --
--- AUTO_INCREMENT for table `wishlist`
+-- AUTO_INCREMENT for table wishlist
 --
-ALTER TABLE `wishlist`
-  MODIFY `wishlist_id` bigint(20) NOT NULL AUTO_INCREMENT;
+ALTER TABLE wishlist
+  MODIFY wishlist_id bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `cart`
+-- Constraints for table cart
 --
-ALTER TABLE `cart`
-  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`) ON DELETE CASCADE;
+ALTER TABLE cart
+  ADD CONSTRAINT cart_ibfk_1 FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE,
+  ADD CONSTRAINT cart_ibfk_2 FOREIGN KEY (item_id) REFERENCES item (item_id) ON DELETE CASCADE;
 
 --
--- Constraints for table `item_images`
+-- Constraints for table item_images
 --
-ALTER TABLE `item_images`
-  ADD CONSTRAINT `item_images_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`) ON DELETE CASCADE;
+ALTER TABLE item_images
+  ADD CONSTRAINT item_images_ibfk_1 FOREIGN KEY (item_id) REFERENCES item (item_id) ON DELETE CASCADE;
 
 --
--- Constraints for table `notifications`
+-- Constraints for table notifications
 --
-ALTER TABLE `notifications`
-  ADD CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE;
+ALTER TABLE notifications
+  ADD CONSTRAINT notifications_ibfk_1 FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE;
 
 --
--- Constraints for table `orders`
+-- Constraints for table orders
 --
-ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE;
+ALTER TABLE orders
+  ADD CONSTRAINT orders_ibfk_1 FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE;
 
 --
--- Constraints for table `order_items`
+-- Constraints for table order_items
 --
-ALTER TABLE `order_items`
-  ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`) ON DELETE CASCADE;
+ALTER TABLE order_items
+  ADD CONSTRAINT order_items_ibfk_1 FOREIGN KEY (order_id) REFERENCES orders (order_id) ON DELETE CASCADE,
+  ADD CONSTRAINT order_items_ibfk_2 FOREIGN KEY (item_id) REFERENCES item (item_id) ON DELETE CASCADE;
 
 --
--- Constraints for table `wishlist`
+-- Constraints for table wishlist
 --
-ALTER TABLE `wishlist`
-  ADD CONSTRAINT `wishlist_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `wishlist_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`) ON DELETE CASCADE;
+ALTER TABLE wishlist
+  ADD CONSTRAINT wishlist_ibfk_1 FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE,
+  ADD CONSTRAINT wishlist_ibfk_2 FOREIGN KEY (item_id) REFERENCES item (item_id) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
