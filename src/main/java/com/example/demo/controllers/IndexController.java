2bcac7d4b32c308268ff732e541448632dad44a7
gitzuzu <Zeina2101569@miuegypt.edu.eg>
2024-03-18 06:39:32 +0200

    navbar - wishlist - cart frontend and routing

--- src/main/java/com/example/demo/controllers/IndexController.java
+++ src/main/java/com/example/demo/controllers/IndexController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -32,5 +34,20 @@ public class IndexController {
         return mav;
     }
     
+    @GetMapping("checkout")
+    public ModelAndView getcart() {
+        ModelAndView mav = new ModelAndView("cart.html");
+        return mav;
+    }
     
+    @GetMapping("product/cart")
+    public ModelAndView getproductcart() {
+        ModelAndView mav = new ModelAndView("cart.html");
+        return mav;
+    }
+    @GetMapping("product/wishlist")
+    public ModelAndView getproductwishlist() {
+        ModelAndView mav = new ModelAndView("wishlist.html");
+        return mav;
+    }
 }
