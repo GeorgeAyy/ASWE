514d3ec553705370053addc1f6b1c674b4f07d2f
dodo <nader.amir02@gmail.com>
2024-04-23 15:33:43 +0200

    Added /login and /signup routes (skipping the /auth) again because tailwind won't apply on /something/something

--- src/main/java/com/example/demo/controllers/IndexController.java
+++ src/main/java/com/example/demo/controllers/IndexController.java
@@ -1,17 +1,21 @@
 package com.example.demo.controllers;
 
 
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.repositories.UserRepository;
 
 
-
+import com.example.demo.models.User;
 
 @Controller
 @RequestMapping("/")
@@ -22,6 +26,40 @@ public class IndexController {
         ModelAndView mav = new ModelAndView("index.html");
         return mav;
     }
+
+    @Autowired
+    private UserRepository UserRepository;
+
+    @GetMapping("/signup")
+    public ModelAndView signup() {
+        ModelAndView mav = new ModelAndView("signup.html");
+        User user = new User();
+        mav.addObject("signupRequest", user);
+        return mav;
+    }
+
+    @GetMapping("/login")
+    public ModelAndView login() {
+        ModelAndView mav = new ModelAndView("login.html");
+        User newUser = new User();
+        mav.addObject("user", newUser);
+        return mav;
+    }
+    
+    @PostMapping("/login")
+    public RedirectView getuser(@RequestParam("email") String email,
+    @RequestParam("userPassword") String userPassword) {
+        
+        User dbUser= this.UserRepository.findByEmail(email);
+        Boolean isPassword = BCrypt.checkpw(userPassword, dbUser.getUserPassword());
+        if (isPassword) {
+       
+        return new RedirectView("/");}
+        else{
+            return new RedirectView("/auth/login");
+        }
+    }
+
     @GetMapping("productlist")
     public ModelAndView getproductlist() {
         ModelAndView mav = new ModelAndView("productList.html");
