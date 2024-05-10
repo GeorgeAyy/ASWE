848f78276c40e40f7b118a0d6c4722ad2dad5fb3
dodo <nader.amir02@gmail.com>
2024-05-10 18:36:50 +0300

    Some custom colors in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./src/main/resources/templates/**/*.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#5a5a5a',
+        'secondary': '#999999',
+        'button': '#111827',
+        'button-hover': '#1f2937',
+        'input': '#f9fafb',
+        'input-hover': '#f3f4f6',
+        'input-focus': '#e5e7eb',
+      },
+    },
   },
   plugins: [],
 }
