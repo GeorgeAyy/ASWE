5b0cc80c9b45af062b98c232424eab07f6688c65
George Aziz <123029529+GeorgeAyy@users.noreply.github.com>
2024-05-27 16:51:18 +0300

    added pagination and also fixed responsiveness

--- src/main/java/com/example/demo/repositories/ItemRepository.java
+++ src/main/java/com/example/demo/repositories/ItemRepository.java
@@ -1,7 +1,8 @@
 package com.example.demo.repositories;
 
 import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -20,8 +21,9 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
                         "AND (:category IS NULL OR i.itemCategory = :category) " +
                         "AND (:minPrice IS NULL OR i.itemPrice >= :minPrice) " +
                         "AND (:maxPrice IS NULL OR i.itemPrice <= :maxPrice)")
-        List<Item> searchItems(@Param("title") String title,
+        Page<Item> searchItems(@Param("title") String title,
                         @Param("category") String category,
                         @Param("minPrice") Double minPrice,
-                        @Param("maxPrice") Double maxPrice);
+                        @Param("maxPrice") Double maxPrice,
+                        Pageable pageable);
 }
