2363f54443a336e96608edcf9f1ad3ccd104ee78
dodo <nader.amir02@gmail.com>
2024-05-29 00:28:51 +0300

    Added some frontend validation, and animations :D

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,22 @@ module.exports = {
         'input-focus': '#e5e7eb',
         'confirm': '#31c07e',
       },
+      keyframes: {
+        pulse: {
+          '0%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(1.1)' },
+          '100%': { transform: 'scale(1)' },
+        },
+        shake: {
+          '0%, 100%': { transform: 'translateX(0)' },
+          '25%, 75%': { transform: 'translateX(-10px)' },
+          '50%': { transform: 'translateX(10px)' },
+        }
+      },
+      animation: {
+        pulse: 'pulse 2s infinite',
+        shake: 'shake 0.5s',
+      }
     },
   },
   plugins: [],
